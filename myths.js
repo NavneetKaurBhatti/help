4ff4f3942e6a0b4e472f4c38606325a7008028d0
heta08 <68490383+heta08@users.noreply.github.com>
2020-12-12 00:58:09 +0530

    final javascript files

--- myths.js
+++ myths.js
@@ -12,3 +12,26 @@ function search_myths() {
         } 
     } 
 } 
+
+/* function for showing survey window*/
+function popUp(url) {
+    popUpWindow = window.open("survey.html",'popUpWindow','height=700,width=900,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+}
+
+/* for generating dynamic questions*/
+var buttons = document.getElementsByTagName('button'),
+  questions = document.getElementsByTagName('div');
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.addEventListener('click', function() {
+    var target = this.getAttribute('data-next');
+    for (var j = 0; j < questions.length; j++) {
+      var question = questions[j];
+      if (question.id == target) {
+        question.classList.add('show');
+      } else if (question.classList.contains('show')) {
+        question.classList.remove('show');
+      }
+    }
+  });
+}
